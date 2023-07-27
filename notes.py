4cf95ba8b00a52f969a919312042d68827eb96b2
ivasenko <ivasenko98@list.ru>
2023-07-27 20:52:06 +0300

    help add

--- notes.py
+++ notes.py
@@ -8,7 +8,8 @@ from datetime import datetime
 def run():
     flag = True
     while flag:
-        print("Введите команду (add, list, open 'название заметки', dell 'название заметки', help, exit)")
+        print ("\nГлавное меню\n--------------------------------------------------------")
+        print("Введите команду (add, list, open 'название заметки', help, exit)")
         command = input(":").strip().lower()
         if command == "exit":
             return
@@ -24,6 +25,8 @@ def command_controller(command):
         show_notes_list(command)
     elif command[0] == "open":
         open_note(command)
+    elif command[0] == "help":
+        help()
     else:
         show_error_message("Неверная команда '" +
                            command[0]+"'. Чтобы открыть справку введите 'help'")
@@ -75,6 +78,7 @@ def show_notes_list(command):
             print(f"{counter}.  {current_note.get('title')}")
             print(f"Дата создания:  {current_note.get('create_date')}\n")
             counter += 1
+        print("----------------------------------------\n")
     elif command[1] in ["-du", "-dua", "-dd", "-dda", "-dr", ]:
         notes_list_filter(notes, command[1])
 
@@ -171,9 +175,9 @@ def date_is_ok(date):
 
     return True
 
+
+
 # вернуть отфильтрованный список
-
-
 def get_filtred_list(notes, filtration_type, first_date, second_date=0):
     if filtration_type == "u":
         min_date = first_date
@@ -198,8 +202,8 @@ def get_filtred_list(notes, filtration_type, first_date, second_date=0):
         if first_date_more(first_note_create_date, max_date):
             show_error_message("Нет записок подходящих под условие")
             return None
+        
         if not first_date_more(last_note_create_date, max_date):
-
             result_notes_list=notes
             for i in range(len(result_notes_list)//2):
                 result_notes_list[i], result_notes_list[-i-1] =result_notes_list[-i-1], result_notes_list[i]
@@ -245,7 +249,7 @@ def first_date_more(first_date, second_date):
 # бинарный поиск минимальной даты
 def binary_search_min(notes, required_date):
     low = 0
-
+    
     hight = len(notes)-1
 
     while low <= hight:
@@ -291,26 +295,84 @@ def open_note(command):
     
     if len(command)>2:
         command[1]=" ".join(command[1:])
-    if len(command)!=2:
+    if len(command)<2:
         show_error_message("Неверный формат команды. для стравки введите help")
     
     notes = get_note_list()
     result_search=list()
-    print (command[1][1:-1].strip())
-    for i in notes:
-        if i.get("title").strip()==command[1][1:-1].strip():
-            result_search.append(i)
-
+    result_search_indexes=list()
+    
+    for i in range(len(notes)):
+        if notes[i].get("title").strip()==command[1][1:-1].strip():
+            result_search.append(notes[i])
+            result_search_indexes.append(i)
+  
     if len(result_search)==0:
         show_error_message("Ошибка!!! записка "+command[1]+" ненайдена. чтобы посмотреть все записки введите 'list'")
     elif    len(result_search)>1:
         print (f"\nНайдено {len(result_search)} {'записки' if len(result_search)<=4 else 'записок'}.\n--------------------------------------")
         counter = 1
         for i in result_search:
-            print(f"Записка №{counter}")
+            print(f"Записка №{counter}\n----")
             print_note(i)
             counter+=1
+        flag=True
+        while flag:
+            print("Введите 'edit + №записки'- чтобы редактровать записку.'dell + №записки' - чтобы удалить записку. 'q'-вернуться в меню")
+            user_input = input(":").strip().lower()
+            if user_input == "q":
+                return
+            elif user_input.split()[0] == "edit":
+                if len(user_input.split())!=2:
+                    show_error_message("Ошибка!!! неверный номер заметки")
+                else:
+                    input_is_ok = True
+                    note_number = user_input.split()[1]
+                    try:
+                        note_number = int(note_number)
+                    except Exception:
+                        show_error_message(
+                            "Ошибка!!! номер заметки должен быть числом!")
+                        input_is_ok=False
+            
+                if  input_is_ok:             
+                    edit_note(notes, result_search_indexes[note_number-1])
+                    flag = False
+
+            elif user_input.split()[0] == "dell":
+                if len(user_input.split())!=2:
+                    show_error_message("Ошибка!!! неверный номер заметки")
+                else:
+                    input_is_ok = True
+                    note_number = user_input.split()[1]
+                    try:
+                        note_number = int(note_number)
+                    except Exception:
+                        show_error_message(
+                            "Ошибка!!! номер заметки должен быть числом!")
+                        input_is_ok=False
+            
+                if  input_is_ok:
+                    dell_confirm = False
+
+                    input_is_ok = True
+                    while input_is_ok:
+                        confrim=input("подтвердить удаление заметки?(yes/no):  ").strip().lower()
+                        if confrim == "yes":
+                            dell_confirm = True
+                            input_is_ok=False
+                        elif confrim=="no":
+                            input_is_ok=False
+                        else:
+                            show_error_message("Oшибка!!! чтобы подтвердить удаление введите 'yes', чтобы отменить введите 'no'") 
+
+                    if dell_confirm:       
+                        dell_note(notes, result_search_indexes[note_number-1])
+                    flag = False
+            else:
+                show_error_message("Ошибка!!! неверный формат команды")   
     else: 
+        print ("\n--------------------------------------------------------")
         print_note(result_search[0])
         flag = True
         while flag:
@@ -318,10 +380,110 @@ def open_note(command):
             user_input = input(":").strip().lower()
             if user_input == "q":
                 return
+            
+            elif user_input == "edit":              
+                edit_note(notes, result_search_indexes[0])
+                flag = False
+
+            elif user_input.split()[0] == "dell":                
+                dell_confirm = False
+                input_is_ok = True
+
+                while input_is_ok:
+                    confrim=input("подтвердить удаление заметки?(yes/no):  ").strip().lower()
+                    if confrim == "yes":
+                        dell_confirm = True
+                        input_is_ok=False
+                    elif confrim=="no":
+                        input_is_ok=False 
+                    else:
+                        show_error_message("Oшибка!!! чтобы подтвердить удаление введите 'yes', чтобы отменить введите 'no'")
+                        
+
+                if dell_confirm:       
+                    dell_note(notes, result_search_indexes[0])
+                flag = False     
+            
+            
+            
+
+                  
+ # редактировать заметку
+def edit_note (notes, index):
+    edited=False
+    flag =True
+    while flag:
+        print ('\nРедактирование заметки:\n--------------------------------------------------------')
+        print("1. Чтобы отредактировать заголовок - введите 'title'")
+        print("2. Чтобы отредактировать текст заметки - введите 'text'")
+        print("3. Для сохранения - введите 'save'")
+        print("3. Чтобы вернуться - вветите 'q'")
+        
+        new_title_note = notes[index].get("title")
+        new_text_note = notes[index].get("title")
+        
+        command = input(":").strip().lower()
 
         
-    
+        if command=="q":
+            flag = False
+            if edited:
+                input_is_ok=True
+                while input_is_ok:                    
+                    save_or_no = input("Вы не сохранили изменения. отменить все измениния(yes/no):  ").strip().lower()
 
+                    if save_or_no == "yes":
+                        return
+                    elif save_or_no == "no":
+                        input_is_ok=False
+                        flag=True
+                    else:
+                        show_error_message("Ошибка!!! Введите 'yes' чтобы выйти без сохранения. или 'no' чтобы продолжить редактрование")
+                    
+            
+        elif command=="title":
+            new_title_note =  input("Введите новый заголовок:\t")
+            notes[index]["title"]=new_title_note
+            edited=True
+        elif command =="text":
+            print("\nВведите новый текст заметки (чтобы завешить редактирование введите на новой строке 'Ctrl+z' для Windows или 'Ctrl+d' для Linux/Mac):")
+            new_text_note = sys.stdin.readlines()
+            notes[index]["text"]=new_text_note
+            edited=True
+        elif command=='save':
+            if not edited:
+                return
+            flag=False
+
+            new_date_edited = datetime.now()
+            new_date_edited = new_date_edited.strftime("%d.%m.%Y %H:%M:%S")
+            
+
+            notes[index]["edit_date"]=new_date_edited
+
+            with (open("test.json", mode="w"))as f:
+                json.dump(notes, f, indent=2)
+                f.close
+        else: 
+            show_error_message("Ошибка!!! неверная команда!")    
+
+        
+
+# удалить заметку
+def dell_note(notes, index):
+    notes.pop(index)
+    title = notes[index].get("title")
+    with (open("test.json", mode="w")) as f:
+        json.dump(notes, f, indent=2)
+        f.close
+    print ("-----------------------------------------")
+    print(f"записка {title} удалена")
+    print ("-----------------------------------------\n")
+
+
+    
+    
+# напечатать заметку 
 def print_note(note):
     print(f"Заголовок:  {note.get('title')}")        
     print(f"Дата создания:  {note.get('create_date')}")
@@ -337,12 +499,13 @@ def print_note(note):
 def save_note(title_note, text_note, create_date, edit_date):
     data = get_note_list()
 
-    new_title_json = {"title": title_note, "text": text_note,
+    new_note_json = {"title": title_note, "text": text_note,
                       "edit_date": edit_date, "create_date": create_date}
-    data.append(new_title_json)
+    data.append(new_note_json)
 
     with (open("test.json", mode="w")) as f:
         json.dump(data, f, indent=2)
+        f.close
 
 
 
@@ -358,6 +521,23 @@ def get_note_list():
     else:
         show_error_message("Список заметок пуст")
     return data
+
+
+# вывести справку.
+def help():
+    print("\nСправка по командам\n====================================================")
+    print("add - создать новую заметку:\n\tСинтаксис:  add\nключи не предусмотренны")
+
+    print("\nlist - вывести список заметок:\n\tСинтаксис:  list -key (Пример: list -dua)")
+    print("\t  Спосок ключей:\t  без ключа - вывести все заметки по убыванию даты создания ")
+    print("\t  -dua - вывести все заметки по возрастанию даты созания")
+    print("\t  -du - вывести все заметки созданные после определенной даты ")
+    print("\t  -dda - вывести все заметки по убыванию даты созания ")
+    print("\t  -dd - вывести все заметки озданные раньше определенной даты ")
+
+    print("\nopen - открыть заметку:\n\tСинтаксис:  open 'заголовок заметки' (Пример: open 'title')\n\t  ключи не предусмотренны")
+
+    print("\nexit - завершить работу:\n\tСинтаксис:  exit\nключи не предусмотренны")    
 
 
 # Вывести сообщение об ошибке
